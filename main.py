ef727dc2f4df27fd367bb2da23e6eacf3d05aaa7
rRokist <levking.shcherbakov@gmail.com>
2024-04-11 22:09:44 +0300

    Login form added

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# class LoginForm(FlaskForm):
+#    email = EmailField('Почта', validators=[DataRequired()])
+#    password = PasswordField('Пароль', validators=[DataRequired()])
+#    remember_me = BooleanField('Запомнить меня')
+#    submit = SubmitField('Войти')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return 1
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 @app.route('/')
 def index():
     return render_template(f"mainpage/mainpage.html", title="ToDoGenius")
+
+
 @app.route('/login')
-def index():
-    return render_template(f"login/login.html", title="ToDoGenius")
+def login():
+
+    return render_template('login/login.html', title='Авторизация')
 
 
 if __name__ == '__main__':
