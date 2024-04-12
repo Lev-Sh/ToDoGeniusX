47a7092b0e0f22acf6736b36bd4bf3b768d9b17b
Dmitry <dmitriyskorohodov@yandex.ru>
2024-04-12 15:45:49 +0300

    REGISTRATION IS WORKING NOW!!!!

--- data/users.py
+++ data/users.py
@@ -2,14 +2,15 @@ import datetime
 
 import sqlalchemy as sa
 from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sa.Column(sa.Integer,
                    primary_key=True, autoincrement=True)
-    nickname = sa.Column(sa.String)
+    nickname = sa.Column(sa.String, nullable=True)
     name = sa.Column(sa.String, nullable=True)
     birthday = sa.Column(sa.Integer, nullable=True)
     email = sa.Column(sa.String, index=True, unique=True)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import hashlib
 import bcrypt
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_login import LoginManager, login_user, current_user
 
-from data.__all_models import *
+from data.users import User
 from data.db_session import global_init, create_session
 from data import _utils
 
@@ -26,33 +26,53 @@ def index():
     return render_template(f"mainpage/mainpage.html", title="ToDoGenius")
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = _utils.SignUpForm()
-    if form.validate_on_submit() and form.password.data == form.confirm_password.data:
+    if form.validate_on_submit():
+        if not form.password.data == form.confirm_password.data:
+            pass
+            # return render_template('login/login.html',
+            #                        message='Введённые пароли не совпадают',
+            #                        form=form)
         session = create_session()
-        user = users.User()
+        user = User()
 
         salt = bcrypt.gensalt()
         hashed_pass = hashlib.md5((form.password.data + salt.decode()).encode()).digest()
 
-        user.nickname = form.nickname.data
-        user.birthday = form.birthday.data
-        if form.name.data:
-            user.name = form.name.data
+        # user.nickname = form.nickname.data
+        # user.birthday = form.birthday.data
+        # if form.name.data:
+        #     user.name = form.name.data
         user.email = form.email.data
         user.hashed_password = hashed_pass
         user.salt = salt
 
         session.add(user)
         session.commit()
+
         login_user(user, remember=form.remember_me.data)
+
+        return redirect('/')
+
+    return render_template('login/login.html', form=form, title='Регистрация')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
-    return render_template('login/login.html', title='Авторизация')
+    form = _utils.LoginForm()
+    if form.validate_on_submit():
+        session = create_session()
+        password = form.password.data
+        user = session.query(User).filter(User.email == form.email.data).first()
+        salt = user.salt
+        if hashlib.md5((password + salt.decode()).encode()).digest() == user.hashed_password:
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login/login.html', message='Введён неправильный пароль',
+                               title='Авторизация', form=form)
+    return render_template('login/login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
